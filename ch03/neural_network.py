e6692ad0929faaf2be5cc1f4c5ffd83ffb1f4a7f
abkeito <abkeito@yahoo.co.jp>
2025-04-25 23:12:18 +0900

    feat: chapter3 halfway there

---
+++ ch03/neural_network.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 3.2 活性化関数
+# Numpyで一度ブーリアン値を作成してから、astypeでintに変換する
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int64)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def identity_function(x):
+    return x
+
+x = np.arange(-5.0, 5.0, 0.1)
+y_step = step_function(x)
+y_sigmoid = sigmoid(x)
+y_relu = relu(x)
+plt.plot(x, y_step, label="step function")
+plt.plot(x, y_sigmoid, label="sigmoid")
+plt.plot(x, y_relu, label="relu")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.ylim(-0.1, 1.1)
+plt.legend()
+plt.savefig("activation_function.png")
+
+# 3.3 多次元配列の計算
+X = np.array([1, 2])
+print(X.shape)
+W = np.array([[1,3,5], [2,4,6]])
+print(W.shape)
+Y = np.dot(X, W)
+print(Y)
+
+# 3.4 3層ニューラルネットワークの実装
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network["W2"] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
+
+# 3.5 出力層の設計
+def soft_max(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+# 3.6 手書き数字認識
+import sys, os
+sys.path.append(os.path.abspath(os.pardir))
+print(sys.path)
+from dataset_zero.mnist import load_mnist
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
